c6e05b63d2f38e0f19c140d22f8698351a969199
jumgal <cumgal@gmail.com>
2023-06-12 22:44:02 -0500

    add text learn react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         It is a feature branch
+         It is a feature branch. Learn React
         </a>
       </header>
     </div>
